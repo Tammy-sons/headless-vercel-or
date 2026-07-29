1d77efe6e27a4559fc84e6f1b6abdd082541ea61
thewebguy91-del <thewebguy91@gmail.com>
2026-07-30 00:18:50 +0500

    fix: re-trigger ISR generation for categories

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,5 +4,6 @@ import { NextRequest, NextResponse } from 'next/server';
 export const runtime = 'edge';
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
+  console.log('Busting Cache');
   return revalidate(req);
 }
