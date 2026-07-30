5d46c6eb8dd0ee496b5d0cffb65ef95a83989a8c
thewebguy91-del <thewebguy91@gmail.com>
2026-07-31 02:36:58 +0500

    'sitemap-fix6'

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -4,10 +4,10 @@ const rawUrl = process.env.NEXT_PUBLIC_SITE_URL || process.env.VERCEL_URL || 'lo
 const cleanHost = rawUrl.replace(/^https?:\/\//, '').replace(/\/$/, '');
 const baseUrl = cleanHost.includes('localhost') ? `http://${cleanHost}` : `https://${cleanHost}`;
 
-export const revalidate = 3600;
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
-    const sitemaps = ['sitemap-pages.xml', 'sitemap-products.xml', 'sitemap-blog.xml'];
+    const sitemaps = ['sitemap-pages.xml', 'sitemap-categories.xml', 'sitemap-products.xml', 'sitemap-blog.xml'];
 
     const body = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
